61a9be0df5e09087a4b915c55b7bec193b80ffff
Moffein <22742925+Moffein@users.noreply.github.com>
2025-08-06 12:46:30 -0500

    Updated some names

--- SneedHooks/GetStatCoefficients.cs
+++ SneedHooks/GetStatCoefficients.cs
@@ -6,12 +6,12 @@ namespace SneedHooks
     //Almost every item ends up having an inventory check in GetStatCoefficients, so this exists to just skip that step.
     public static class GetStatCoefficients
     {
-        public delegate void HandleStatsInventory(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args, Inventory inventory);
-        public static HandleStatsInventory HandleStatsInventoryActions;
+        public delegate void GetStatInventory(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args, Inventory inventory);
+        public static GetStatInventory GetStatInventoryActions;
 
         internal static void RecalculateStatsAPI_GetStatCoefficients(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args)
         {
-            if (sender.inventory) HandleStatsInventoryActions?.Invoke(sender, args, sender.inventory);
+            if (sender.inventory) GetStatInventoryActions?.Invoke(sender, args, sender.inventory);
         }
     }
 }
--- SneedHooks/RecalculateStats.cs
+++ SneedHooks/RecalculateStats.cs
@@ -4,13 +4,13 @@ namespace SneedHooks
 {
     public static class RecalculateStats
     {
-        public delegate void HandleRecalculateStatsInventory(CharacterBody self, Inventory inventory);
-        public static HandleRecalculateStatsInventory HandleRecalculateStatsInventoryActions;
+        public delegate void RecalculateStatsInventory(CharacterBody self, Inventory inventory);
+        public static RecalculateStatsInventory RecalculateStatsInventoryActions;
 
         internal static void CharacterBody_RecalculateStats(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
         {
             orig(self);
-            if (self.inventory) HandleRecalculateStatsInventoryActions?.Invoke(self, self.inventory);
+            if (self.inventory) RecalculateStatsInventoryActions?.Invoke(self, self.inventory);
         }
     }
 }
