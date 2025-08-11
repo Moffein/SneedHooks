b3aeeac3f9571059005b726e5c4ec33ea433b114
Moffein <22742925+Moffein@users.noreply.github.com>
2025-08-10 19:48:37 -0500

    removed r2api dependency

--- SneedHooks/SneedHooksPlugin.cs
+++ SneedHooks/SneedHooksPlugin.cs
@@ -1,18 +1,14 @@
 ﻿using BepInEx;
 using System.Security.Permissions;
 using System.Security;
-using R2API.Utils;
-using R2API;
 using UnityEngine;
+using System;
 
 [module: UnverifiableCode]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 namespace SneedHooks
 {
-    [BepInDependency(R2API.R2API.PluginGUID)]
-    [BepInDependency(R2API.RecalculateStatsAPI.PluginGUID)]
     [BepInPlugin("com.RiskyLives.SneedHooks", "SneedHooks", "1.0.0")]
-    [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     public class SneedHooksPlugin : BaseUnityPlugin
     {
         private void Awake()
@@ -20,5 +16,13 @@ namespace SneedHooks
             On.RoR2.GlobalEventManager.ProcessHitEnemy += ProcessHitEnemy.GlobalEventManager_ProcessHitEnemy;
             IL.RoR2.HealthComponent.TakeDamageProcess += ModifyFinalDamage.HealthComponent_TakeDamageProcess;
         }
+    }
+}
+
+namespace R2API.Utils
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class ManualNetworkRegistrationAttribute : Attribute
+    {
     }
 }
